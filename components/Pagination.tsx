a083638b4ce71a5ae53a260c28840204965cee7c
Kuvam Bhardwaj <kuvamyash@gmail.com>
2021-11-10 13:08:30 +0530

    made pagination element

---
+++ components/Pagination.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react"
+
+interface PaginationProps {
+    fetchUrl: (page: number) => string
+    renderElement: (data: any) => React.ReactElement
+    dataListKey: string
+    dataTotalKey: string
+    footerNav: (pageNums: Array<number>, page: number, setPage: React.Dispatch<React.SetStateAction<number>>) => React.ReactElement
+    
+    className?: string
+    placeholder?: React.ReactElement
+}
+
+export default function Pagination({
+    className='', fetchUrl, renderElement, placeholder, dataListKey, dataTotalKey, footerNav
+}: PaginationProps) {
+
+    const [paginatedData, setData] = useState(null)
+    const [page, setPage] = useState(1)
+    const [pages, setPages] = useState<number[]>([])
+
+    useEffect(() => {
+        fetch(fetchUrl(page)).then(res => res.json())
+        .then(data => {
+            setData(data[dataListKey])
+            // dataListKey is the variable to reference the actual list of items which is to be rendered.
+            // Here, the data is given back as:
+            //     { currentPage: number, total: number, items: [{ name, agem gender }...] }
+            
+            let pagesList = []
+            for (let i = 1; i <= data[dataTotalKey]; i++) {
+                pagesList.push(i)
+            }
+            setPages(pagesList)
+        })
+
+    }, [page])
+    
+    if (paginatedData) return (<>
+        <section className={className}>
+            {(paginatedData as Array<object>).map((element: any) => renderElement(element))}
+        </section>
+
+        {footerNav(pages, page, setPage)}
+    </>)
+
+    return (<>
+        {placeholder || <p>Loading...</p>}
+    </>)
+
+}
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,46 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
+// type Data = {
+//   name: string
+// }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+	const pageLength = 5
+	const sampleData = [
+		{ name: 'Sabrina', age: 21, gender: 'female' },
+		{ name: 'Eric', age: 21, gender: 'male' },
+		{ name: 'Lena', age: 21, gender: 'female' },
+		{ name: 'Lusha', age: 21, gender: 'female' },
+		{ name: 'Leon', age: 21, gender: 'male' },
+		{ name: 'Angela', age: 21, gender: 'female' },
+		{ name: 'Mia', age: 21, gender: 'female' },
+		{ name: 'Jenny', age: 21, gender: 'female' },
+		{ name: 'Debbie', age: 21, gender: 'female' },
+		{ name: 'Karen', age: 21, gender: 'female' },
+		{ name: 'Mileva', age: 21, gender: 'female' },
+		{ name: 'Anne', age: 21, gender: 'female' },
+		{ name: 'Dan', age: 21, gender: 'male' },
+		{ name: 'John', age: 21, gender: 'male' },
+	]
+
+	const { page } = req.query
+	const pageNum = parseInt(page as string)
+
+	let responseData
+	if (page) {
+		responseData = sampleData.slice((pageNum-1) * pageLength, pageNum * pageLength)
+	}
+
+	res.status(200).json({
+		page: pageNum, total: Math.ceil(sampleData.length / pageLength),
+		items: responseData
+	})
 }
